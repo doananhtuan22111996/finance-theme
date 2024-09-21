2cbecca17788a820e888743cb008f998ba12659d
Jason Doan <35907749+doananhtuan22111996@users.noreply.github.com>
2024-09-21 16:32:34 +0700

    feat: init theme (#1)

---
+++ buildSrc/src/main/java/vn/finance/src/Configs.kt
@@ -0,0 +1,17 @@
+package vn.finance.src
+
+import org.gradle.api.JavaVersion
+
+object Configs {
+    const val namespace = "vn.finance.theme"
+    const val minSdk = 24
+    const val targetSdk = 34
+    const val compileSdk = 34
+    const val jvmTarget = "17"
+    const val kotlinCompilerExtensionVersion = "1.5.14"
+    val javaVersion = JavaVersion.VERSION_17
+    const val mavenDomain = "https://maven.pkg.github.com"
+
+}
+
+
